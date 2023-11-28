aca555885f99b437042c65d381e6d8774d77052d
Filipe <filiperdacas@gmail.com>
2023-11-27 22:36:16 -0300

    inserted router tasks

---
+++ routes/tasks.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const router = express.Router();
+const sequelize = require('../db');
+const Usuario = require('../models/tarefa');
+sequelize.sync();  
+//GET Retorna tarefas com paginação e ordenação
+router.get('/', async (req, res) => {
+    const { page = 1, limit = 10 } = req.query;
+    sequelize.query(`SELECT * FROM tarefas ORDER BY updatedAt DESC LIMIT ? OFFSET ?`,
+        { replacements: [parseInt(limit), (page - 1) * parseInt(limit)] }
+    )
+    .then(([results, metadata]) => {
+        res.json(results);
+    }).catch((error) => {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    });
+});
+
+//GET Consulta uma tarefa pelo ID
+router.get('/:id', async (req, res) => {
+    sequelize.query(`SELECT * FROM tarefas WHERE id = ?`, { replacements: [req.params.id] })
+    .then(([results, metadata]) => {
+        if (results.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: "tarefas não encontrado",
+            });
+        } else {
+            res.json({
+                success: true,
+                task: results[0],
+            });
+        }
+    }).catch((error) => {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    });
+});
+
+//POST Cria uma tarefa
+router.post('/', async (req, res) => {
+    sequelize.query(`INSERT INTO tarefas (titulo, descricao, status,data_criacao, data_limite, createdAt, updatedAt) VALUES (?, ?, ?, ?, ?, ? , ?)`,
+        { replacements: [req.body.titulo, req.body.descricao, req.body.status, new Date(),new Date(),new Date(),new Date()] }
+    )
+    .then(([results, metadata]) => {
+        res.status(201).json({
+            success: true,
+            message: "tarefa criada com sucesso",
+        });
+    }).catch((error) => {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    });
+});
+
+//PUT Atualiza uma tarefa pelo ID
+router.put('/:id', async (req, res) => {
+    sequelize.query(`UPDATE tarefas SET descricao = ? WHERE id = ?`,
+        { replacements: [req.body.descricao, req.params.id] }
+    )
+    .then(([results, metadata]) => {
+        if (metadata.affectedRows === 0) {
+            res.status(404).json({
+                success: false,
+                message: "tarefa não encontrada",
+            });
+        } else {
+            res.json({
+                success: true,
+                message: "tarefa atualizada com sucesso",
+            });
+        }
+    }).catch((error) => {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    });
+});
+
+//DELETE Deleta uma tarefa pelo ID
+router.delete('/:id', async (req, res) => {
+    sequelize.query(`DELETE FROM tarefas WHERE id = ?`, { replacements: [req.params.id] })
+    .then(([results, metadata]) => {
+        if (metadata.affectedRows === 0) {
+            res.status(404).json({
+                success: false,
+                message: "tarefa não encontrada",
+            });
+        } else {
+            res.json({
+                success: true,
+                message: "tarefa deletada com sucesso",
+            });
+        }
+    }).catch((error) => {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    });
+});
+
+module.exports = router;
